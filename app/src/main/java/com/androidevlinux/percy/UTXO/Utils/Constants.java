a446dc196c34b37a115426547b4bb7b12ca680e2
Prayank23 <prayank23@outlook.com>
2017-11-19 00:38:15 +0530

    Updated 'Read Me'

--- app/src/main/java/com/androidevlinux/percy/UTXO/Utils/Constants.java
+++ app/src/main/java/com/androidevlinux/percy/UTXO/Utils/Constants.java
@@ -5,6 +5,6 @@ package com.androidevlinux.percy.UTXO.Utils;
  */
 
 public class Constants {
-    public static String api_key = "api_key";
-    public static String secret_key = "secret_key";
+    public static String api_key = "66453fbad84a44ffbd565481addb3d16";
+    public static String secret_key = "f24e94a1390bbd90d0159818648c11c15276db5c276230bcb1d271110b77f44c";
 }
